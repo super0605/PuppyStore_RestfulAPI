e2723d8b46222e5ae0d85459b9e410ce2d5b6f79
super0605 <dream.dev1025@gmail.com>
2020-03-07 15:59:37 +0800

    Add puppies model and unit test for puppies model

--- puppy_store/puppy_store/settings.py
+++ puppy_store/puppy_store/settings.py
@@ -37,7 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'puppies',
+    'rest_framework'
 ]
+
+# REST Framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -73,10 +83,21 @@ WSGI_APPLICATION = 'puppy_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'puppy_store',
+        'USER': 'macdev',     # <your-user>
+        'PASSWORD': '', # <your-password>
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
