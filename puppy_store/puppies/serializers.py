88af57443aee8fa7f3fb8a2b3056ef0579012d34
super0605 <dream.dev1025@gmail.com>
2020-03-08 14:34:35 +0800

    Add puppy id in serializer

--- puppy_store/puppies/serializers.py
+++ puppy_store/puppies/serializers.py
@@ -5,4 +5,4 @@ from .models import Puppy
 class PuppySerializer(serializers.ModelSerializer):
     class Meta:
         model = Puppy
-        fields = ('name', 'age', 'breed', 'color', 'created_at', 'updated_at')
+        fields = ('id', 'name', 'age', 'breed', 'color', 'created_at', 'updated_at')
